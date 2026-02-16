dc753aa519890214e719b8d015c616b92b66a5d2
“Mariane <marianessgaspar@gmail.com>
2026-02-16 11:17:08 -0300

    Projeto inicial - com docker no banco

---
+++ src/main/java/com/mariane/api_finance/ApiFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.mariane.api_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariane/api_finance/ApiFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariane.api_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
